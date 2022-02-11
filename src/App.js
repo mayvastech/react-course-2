84b4bf38a18e257ca401d4bfc385ec4ffb33584e
mayvastech <65177211+mayvastech@users.noreply.github.com>
2022-02-11 15:01:05 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavouritesPage from "./pages/Favourites";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <Route path="/">
+        <AllMeetupsPage></AllMeetupsPage>
+      </Route>
+      <Route path="/new-meetup">
+        <NewMeetupPage></NewMeetupPage>
+      </Route>
+      <Route path="/favourites-page">
+        <FavouritesPage></FavouritesPage>
+      </Route>
+
     </div>
   );
 }
 
 export default App;
+
+// Router provides component page navigation via single page interactivity
+// Router watches changes in URL then changes whats visible based on URL
+
+// Initialise router package ensuring it listens to URL changes
+// Define URLS to be supported and pages to be loaded for different URL's
+
+// Route component defines different paths in the URL listening to and which component to load
+
+// The part after domain name is the path e.g- my-page.com/products
+
+// Path= "/" handles initial default path. Between the tags we add the component to be loaded
